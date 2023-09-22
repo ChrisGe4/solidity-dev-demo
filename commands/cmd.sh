a58b33822c22ee07df83682d797f85cdb587c509
Chris <chris.ge@live.com>
2023-09-21 22:35:32 -0400

    foundary setup done

---
+++ commands/cmd.sh
@@ -0,0 +1,23 @@
+forge create SimpleStorage \
+--rpc-url=http://127.0.0.1:8545 \
+--private-key=""
+
+
+forge script script/DeploySimpleStorage.s.sol \
+--rpc-url=http://127.0.0.1:8545 \
+--broadcast \
+--private-key=""
+
+
+cast --to-base 0x714e1 dec
+
+
+source .env
+forge script script/DeploySimpleStorage.s.sol \
+--rpc-url=${RPC_URL} \
+--broadcast \
+--private-key=${PRIVATE_KEY}
+
+
+cast send 0x8726a3D7fF8dC603d753497FA00b7f7D47b2F3dC "store(uint256)" 100 --rpc-url=${RPC_URL} --private-key=${PRIVATE_KEY}
+cast call 0x8726a3D7fF8dC603d753497FA00b7f7D47b2F3dC "retrieve()" 
